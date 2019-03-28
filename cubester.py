099280c40ee86e851ad46702274c00c53cab852e
Jacob Morris <blendingjake@gmail.com>
2019-03-28 18:36:47 -0400

    add logging, mesh creation, chance data handling, layers are currently off

--- cubester.py
+++ cubester.py
@@ -17,6 +17,7 @@ from operator import itemgetter
 
 import bpy
 from bpy.path import abspath
+from mathutils import Vector
 
 bl_info = {
     "name": "CubeSter",
@@ -30,48 +31,121 @@ bl_info = {
 
 from bpy.types import Scene, Object, Panel, Operator, PropertyGroup, Image
 from bpy.props import PointerProperty, IntProperty, EnumProperty, FloatProperty, StringProperty, BoolProperty, \
-    CollectionProperty
+    CollectionProperty, FloatVectorProperty
 from bpy.utils import register_class, unregister_class
 from bpy import app
 from pathlib import Path
 from typing import List, Tuple
 from os import walk
 from random import uniform
+import bmesh
+import logging
 
 
+logging.basicConfig(level=logging.DEBUG)
 CACHE = {}  # keep track of loaded images data to allow faster creation the second time around
 
 
-def create_random_data(rows: int, columns: int, layer_count) -> List[List[Tuple[list, float]]]:
+def build_bmesh(vertices: list, faces: list) -> bmesh.types.BMesh:
+    logging.debug("BMESH: {} Vertices, {} Faces".format(len(vertices), len(faces)))
+
+    bm = bmesh.new()
+
+    for v in vertices:
+        bm.verts.new(v)
+    bm.verts.ensure_lookup_table()
+
+    for f in faces:
+        bm.faces.new([bm.verts[i] for i in f])
+    bm.faces.ensure_lookup_table()
+
+    return bm
+
+
+def create_block_geometry(height_layers, height_factor: float, xy: float, spacing: float, dims: Tuple[int, int]
+                          ) -> Tuple[list, list]:
+    """
+    Create vertex and face data from the given heights.
+    :param height_layers: The layers of rows of columns of height data
+    :param height_factor: the height of the max value
+    :param xy: the x-y dimensions of each block
+    :param spacing: the x-y space between each block
+    :param dims: the number of rows and columns of blocks
+    :return: vertex positions and faces
+    """
+    verts, faces = [], []
+    rows, cols = dims
+    sx = -((cols*xy) + ((cols - 1)*spacing)) / 2
+    sy = -((rows*xy) + ((rows - 1)*spacing)) / 2
+
+    # vertices
+    p = 0
+    for layer in height_layers:
+        y = sy
+        for row in layer:
+            x = sx
+
+            for bz, height in row:
+                for z in (bz, bz+height):
+                    z *= height_factor
+                    verts += [
+                        (x, y, z), (x, y+xy, z), (x+xy, y+xy, z), (x+xy, y, z)
+                    ]
+
+                x += xy + spacing
+            y += xy + spacing
+
+        # faces
+        for _ in range(rows * cols):
+            faces += [
+                (p, p+1, p+2, p+3), (p+4, p+7, p+6, p+5), (p, p+4, p+5, p+1), (p, p+3, p+7, p+4), (p+3, p+2, p+6, p+7),
+                (p+1, p+5, p+6, p+2)
+            ]
+
+            p += 8
+
+    logging.debug("Blocks: {} Vertices, {} Faces".format(len(verts), len(faces)))
+
+    return verts, faces
+
+
+def create_random_data(rows: int, columns: int, layer_count) -> List[List[List[Tuple[float, float]]]]:
     """
     Create the random heights and colors needed.
     :param rows: the number of rows to create data for
     :param columns: the number of columns to create data for
     :param layer_count: the number of layers to create data for
-    :return: Layers[Rows[Columns[color, height]]]
+    :return: Layers[Rows[Columns[Tuple[z, height]]]
     """
-    layers = []
-    for _ in range(layer_count):
-        layers.append([])
+    height_layers = []
+    for layer_i in range(layer_count):
+        height_layers.append([])
 
         for r in range(rows):
-            layers[-1].append([])
+            height_layers[-1].append([])
 
             for c in range(columns):
                 height = uniform(0, 1)
-                color = [0, 0, 0, 0]  # TODO: allow user to specify how to generate colors
 
-                layers[-1][-1].append((color, height))
+                if layer_i >= 1:
+                    z = height_layers[-2][r][c][0] + height_layers[-2][r][c][1]  # base + height
+                else:
+                    z = 0
+
+                height_layers[-1][-1].append((z, height))
+
+    return height_layers
 
 
-def collect_image_data(image: Image, rows: int, columns: int, multiple_layers: bool) -> List[List[Tuple[list, float]]]:
+def collect_image_data(image: Image, rows: int, columns: int, multiple_layers: bool
+                       ) -> Tuple[List[List[List[list]]], List[List[List[Tuple[float, float]]]]]:
     """
     Collect height and color data from the given image
     :param image: the image to get the data from
     :param rows: the number of rows to collect data on
     :param columns: the number of columns to collect data on
     :param multiple_layers: whether to split the color channels in distinct layers or to combine them into one
-    :return: Layers[Rows[Columns[color, height]]]
+    :return: Layers[Rows[Columns[color]]], Layers[Rows[Columns[Tuple(bottom z, height)]]]
     """
     if image.name in CACHE:
         pixels = CACHE[image.name]
@@ -84,41 +158,111 @@ def collect_image_data(image: Image, rows: int, columns: int, multiple_layers: b
     channels = image.channels
     padding = [0] * (4 - channels)  # amount of padding needed to make sure all colors are RGBA
 
-    layers = []
-    for ch in range(channels if multiple_layers else 1):
-        layers.append([])
+    color_layers = []
+    height_layers = []
+    layer_count = channels if multiple_layers else 1
+    for layer_i in range(layer_count):
+        color_layers.append([])
+        height_layers.append([])
 
         r = 0
         for _ in range(rows):  # manually run loop to ensure that exactly the right number of rows is created
-            layers[-1].append([])
+            color_layers[-1].append([])
+            height_layers[-1].append([])
 
             c = 0
             for _ in range(columns):
-                pos = (((r * width) + c) * channels) + ch
+                pos = (((r * width) + c) * channels) + layer_i
 
-                total = 0
+                height = 0
                 if multiple_layers:
-                    total = pixels[pos]
+                    height = pixels[pos]
 
                     # construct color with value only in proper channel
                     color = [0] * channels
-                    color[ch] = pixels[pos]
+                    color[layer_i] = pixels[pos]
+
+                    # determine z height for bottom of this layer
+                    cur_r, cur_c = len(height_layers[-1]) - 1, len(height_layers[-1][-1]) - 1
+                    if layer_i >= 1:
+                        z = height_layers[-2][cur_r][cur_c][0] + height_layers[-2][cur_r][cur_c][1]  # z + height
+                    else:
+                        z = 0
+
                 else:
+                    z = 0
                     for i in range(pos, pos+channels):
-                        total += pixels[i]
+                        height += pixels[i]
 
                     color = pixels[pos: pos+channels]
 
-                layers[-1][-1].append((color[:channels] + padding, total))
+                color_layers[-1][-1].append(color[:channels] + padding)
+                height_layers[-1][-1].append((z, height))
 
                 c += col_step
             r += row_step
 
-    return layers
+    logging.debug("Image {}: {} Layers, {} Rows, {} Columns".format(image.name, len(color_layers),
+                                                                    len(color_layers[-1]), len(color_layers[-1][-1])))
+
+    return color_layers, height_layers
 
 
 def frame_handler(scene):
     pass
+
+
+def generate_linear_colors(layers: int, rows: int, cols: int, height_layers, min_color: tuple, max_color: tuple,
+                           min_height: float, max_height: float) -> List[List[List[list]]]:
+    """
+    Generate colors based on a linear scale
+    :param layers: the number of layers to generate colors for
+    :param rows: the number of rows to create colors for
+    :param cols: the number of columns to create colors for
+    :param height_layers: the heights for all the layers, rows, and columns to base the colors on
+    :param min_color: the color of the minimum height
+    :param max_color: the color of the maximum height
+    :param min_height: the minimum height
+    :param max_height: the maximum height
+    :return: Layers[Rows[Columns[color]]]
+    """
+    min_vec, max_vec = Vector(min_color), Vector(max_color)
+    slope = (max_vec - min_vec) / (max_height - min_height)
+
+    color_layers = []
+    for l in range(layers):
+        color_layers.append([])
+
+        for r in range(rows):
+            color_layers[-1].append([])
+
+            for c in range(cols):
+                h = height_layers[l][r][c]
+                color_layers[-1][-1].append(list((slope * h) + min_vec))
+
+    return color_layers
+
+
+def invert_all_heights(frames, max_value: float, dims: Tuple[int, int]) -> None:
+    """
+    Invert all the heights
+    :param frames: Frames[Frame Data[Color Layers, Height Layers[Rows[Columns[Tuple[z, height]]]]]]
+    :param max_value: the maximum value possible to allow inversion
+    :param dims: the row and column counts, respectively
+    """
+    rows, cols = dims
+    for frame in frames:
+        height_layers = frame[1]
+        for l in range(len(height_layers)):
+            for r in range(rows):
+                for c in range(cols):
+                    z, height = height_layers[l][r][c]
+                    height = max_value - height  # invert
+
+                    if l >= 1:  # if not the first layer
+                        z = height_layers[-2][r][c][0] + height_layers[-2][r][c][1]  # update z to be above lower level
+
+                    height_layers[l][r][c] = (z, height)
 
 
 # PROPERTIES ----------------------------------------------------
@@ -152,7 +296,7 @@ class CSSceneProperties(PropertyGroup):
 
     mesh_type: EnumProperty(
         name="Mesh Type",
-        items=(("point", "Point", ""), ("block", "Block", ""), ("object", "Object", "")),
+        items=(("point", "Points", ""), ("block", "Blocks", ""), ("object", "Object", "")),
         description="The style of mesh to generate", default="block"
     )
 
@@ -229,6 +373,11 @@ class CSSceneProperties(PropertyGroup):
         description="Height of max value"
     )
 
+    invert_heights: BoolProperty(
+        name="Invert Heights", default=True,
+        description="Make the tallest height actually have the shortest height"
+    )
+
     # LAYERS
     create_layers: BoolProperty(
         name="Create Layers?", default=False
@@ -257,6 +406,16 @@ class CSSceneProperties(PropertyGroup):
         default="from_source"
     )
 
+    linear_min_color: FloatVectorProperty(
+        name="Color of Min Height", default=(0, 0, 0, 1),
+        subtype="COLOR", size=4, description="The color of the minimum height in the mesh"
+    )
+
+    linear_max_color: FloatVectorProperty(
+        name="Color of Max Height", default=(1, 1, 1, 1),
+        subtype="COLOR", size=4, description="The color of the max height in the mesh"
+    )
+
 
 class CSPanel(Panel):
     bl_idname = "OBJECT_PT_cs_panel"
@@ -266,6 +425,11 @@ class CSPanel(Panel):
 
     def draw(self, context):
         layout = self.layout
+
+        if context.mode != "OBJECT":
+            layout.label(text="CubeSter only works in Object Mode")
+            return
+
         props = context.scene.cs_properties
 
         # GENERAL
@@ -325,6 +489,7 @@ class CSPanel(Panel):
         box = layout.box()
         box.label(text="Scaling")
         box.prop(props, "height")
+        box.prop(props, "invert_heights", icon="FILE_REFRESH")
 
         # LAYERS
         box = layout.box()
@@ -342,6 +507,11 @@ class CSPanel(Panel):
         box.label(text="Coloring")
         box.prop(props, "coloring_type")
 
+        if props.coloring_type == "linear":
+            box.separator()
+            box.prop(props, "linear_min_color")
+            box.prop(props, "linear_max_color")
+
         layout.separator()
         layout.operator("object.cs_generate_mesh", icon="SHADERFX")
 
@@ -353,19 +523,61 @@ class CSGenerateMesh(Operator):
 
     def execute(self, context):
         props = context.scene.cs_properties
+        dims = (props.row_count, props.column_count)
 
         frames = []
         if props.source_type == "image":
-            layers = collect_image_data(props.image, props.row_count, props.column_count, props.create_layers)
-            frames.append(layers)
+            colors, heights = collect_image_data(props.image, props.row_count, props.column_count, props.create_layers)
+            frames.append([colors, heights])
         elif props.source_type == "image_sequence":
             for image in props.image_sequence_images:
-                layers = collect_image_data(image.image, props.row_count, props.column_count, props.create_layers)
-                frames.append(layers)
+                colors, heights = collect_image_data(image.image, props.row_count, props.column_count,
+                                                     props.create_layers)
+                frames.append([colors, heights])
         elif props.source_type == "audio":
             pass
         else:
-            layers = create_random_data(props.row_count, props.column_count, 1)
+            heights = create_random_data(props.row_count, props.column_count, props.random_layer_count)
+            frames.append([[], heights])
+
+        logging.debug("{} Frames, {} Layers, {} Rows, {} Columns".format(len(frames), len(frames[0][0]),
+                                                                         len(frames[0][0][0]), len(frames[0][0][0][0])))
+
+        # SCALING
+        if props.source_type == "audio":
+            max_value = props.max_freq
+        else:
+            max_value = 1 if props.create_layers else props.image.channels
+        height_factor = props.height / max_value
+
+        if props.invert_heights:
+            invert_all_heights(frames, max_value, dims)
+
+        # COLORING
+        if props.coloring_type == "from_source":
+            pass
+
+        elif props.coloring_type == "linear":
+            max_height = 1 if props.create_layers else 4
+            for frame in frames:
+                frame[0] = generate_linear_colors(len(frame[1]), props.row_count, props.column_count, frame[1],
+                                                  props.linear_min_color, props.linear_max_color,
+                                                  0, max_height)
+        elif props.coloring_type == "random":
+            pass
+
+        # MESH
+        if props.mesh_type == "point":
+            verts, faces = [], []
+        elif props.mesh_type == "block":
+            verts, faces = create_block_geometry(frames[0][1], height_factor, props.xy_size, props.instance_spacing,
+                                                 dims)
+        else:
+            verts, faces = [], []
+
+        bm = build_bmesh(verts, faces)
+        bpy.ops.mesh.primitive_cube_add()
+        bm.to_mesh(context.object.data)
 
         return {"FINISHED"}
 
